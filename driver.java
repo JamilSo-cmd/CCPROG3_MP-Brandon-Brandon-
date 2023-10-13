6e7cd739091ba4e54cd03a26b7e006ab4881afa8
JamilSo-cmd <brandon_jamil_so@dlsu.edu.ph>
2023-10-13 19:16:54 +0800

    updated driver (Menu Options)

--- driver.java
+++ driver.java
@@ -21,12 +21,36 @@ public class driver {
         System.out.println( "Greetings " + /*player name*/  ", Pick your starter! \n\n[1] Strawander\n\n[2] Brownisaur\n\n[3] Squirpie");
 
         //player picks a starter
-        switch (input){
-            case 1: user.addToInventory(new Creature("Strawander","Fire","A",1,15));
+        while (input < 1 || input > 3){
+            input = scan.nextInt();
+            switch (input){
+                case 1: user.addToInventory(new Creature("Strawander","Fire","A",1,15));
                     user.setActiveCreature(0);
-            case 2: ;
-            case 3: ;
-            default:;
+                break;
+                case 2: user.addToInventory(new Creature("Brownisaur","Grass","D",1,15));
+                    user.setActiveCreature(0);
+                break;
+                case 3: user.addToInventory(new Creature("Squirpie","Water","G",1,15));
+                    user.setActiveCreature(0);
+                default:;
+            }
+        }
+        
+        input = 0;
+        System.out.println( "Menu: \n\n[1] View Inventory\n\n[2] Explore an Area\n\n[3] Evolve Creature\n\n[4] Exit");
+
+        while (input < 1 || input > 4){
+            input = scan.nextInt();
+            switch (input){
+                case 1: ;
+                break;
+                case 2: ;
+                break;
+                case 3: ;
+                break;
+                case 4: ;
+                default:;
+            }
         }
 
     }
